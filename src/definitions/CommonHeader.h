fa6fe902a14cbc80fe8ad96cc919b1a34afd49f3
OceNick <ozcenick@gmail.com>
2022-04-28 06:14:21 -0700

    Add a common header for all files

---
+++ src/definitions/CommonHeader.h
@@ -0,0 +1,11 @@
+//
+// Created by Nikolay on 2022-04-26.
+//
+
+#ifndef GAMEENGINE_COMMONHEADER_H
+#define GAMEENGINE_COMMONHEADER_H
+    #include <cstdint>
+    #include <cassert>
+
+    #include "PrimitiveTypes.h"
+#endif //GAMEENGINE_COMMONHEADER_H
