ccf5f0d11bd60bf54389983999a8813201ff17f4
OceNick <ozcenick@gmail.com>
2022-05-09 20:29:37 -0700

    Add common header for Entity

---
+++ src/entities/CommonEntityHeader.h
@@ -0,0 +1,10 @@
+//
+// Created by Nikolay on 2022-04-28.
+//
+
+#ifndef GAMEENGINE_COMMONENTITYHEADER_H
+    #define GAMEENGINE_COMMONENTITYHEADER_H
+    #include "Id.h"
+    #include "../api/GameEntity.h"
+
+#endif //GAMEENGINE_COMMONENTITYHEADER_H
